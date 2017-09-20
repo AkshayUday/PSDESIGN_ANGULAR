e0b6bd7178b78b7d05c2e6c566be57fc1229a0cd
Nithesh Battepati <nithe.nithesh@gmail.com>
2017-09-20 22:34:14 +0530

    Update globalFeatures.js

--- globalFeatures.js
+++ globalFeatures.js
@@ -1,7 +1,37 @@
-myapp = angular.module('myApp', ['ngSanitize', 'toggle-switch']);
+myapp = angular.module('myApp', ['ngSanitize']);
 myapp.controller('globalCtrl', function($scope, $http) {
 	$scope.myArrayData =[];
+
+	$scope.test= function(v){
+		if (v)
+		{return 'toggle btn btn-default off';}
+		else {
+			{
+				return 'toggle btn btn-primary';
+			}
+		}
+
+	};
+	$scope.toggle = function(id,dat) {
+    $scope.variable = !$scope.variable
+    console.log($scope.variable,id);
+}
+
+$scope.counter = 0;
+		 $scope.change = function(chng,inx) {
+			 if(chng){
+				 $scope.showMessage = inx;
+			 }
+
+			 console.log('changed');
+		 };
+
+$scope.stateChanged = function (qId) {
+   if($scope.myData[qId]){ //If it is checked
+       alert('test');
+   }
+}
   $http.get("./data.json").success(function (response) {
-      $scope.myData = (response.ReadFeaturesResponse.feature); 
+      $scope.myData = (response.ReadFeaturesResponse.feature);
   });
 });
