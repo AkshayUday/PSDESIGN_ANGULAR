46186a050ae2a777382aea8cc631bdfaa176d2f6
Akshay Uday <auakshay123@gmail.com>
2017-10-10 13:59:44 -0400

    Updated 10/10/2017 JS FILE

--- globalFeatures.js
+++ globalFeatures.js
@@ -1,9 +1,14 @@
-myapp = angular.module('myApp', ['ngSanitize']);
-myapp.controller('globalCtrl', function($scope, $http) {
-	$scope.myArrayData =[];
 
+
+
+myapp = angular.module('myApp', ['ngSanitize']);
+myapp.controller('globalCtrl', function($scope,$window, $http) {
+	  
+	console.log($window.cardData)
+	   $scope.myData = ($window.cardData.ReadFeaturesResponse.feature);
+	
 	$scope.test= function(v){
-		if (v)
+		if (!v)
 		{return 'toggle btn btn-default off';}
 		else {
 			{
@@ -18,20 +23,49 @@ myapp.controller('globalCtrl', function($scope, $http) {
 }
 
 $scope.counter = 0;
-		 $scope.change = function(chng,inx) {
-			 if(chng){
+		/* $scope.change = function(chng,inx) {
+			 if(!chng){
 				 $scope.showMessage = inx;
 			 }
 
 			 console.log('changed');
-		 };
+		 };*/
+		 
+		 $scope.change = function(obj, inx) {
+			    if (!obj.enabled) {
+			      $scope.showMessage = inx;
+			    };
 
+			  var Pdata = { name: obj.name, enabled: !obj.enabled, resourceId: obj.featureIdentifier.
+resourceId};
+
+
+			    // Simple POST request example:
+			  $http({
+				  method: 'POST',
+				  url: 'updateGlobalFeatures',
+				  data: Pdata,
+				  headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				}).then(function successCallback(response) {
+				  console.log('success response', response)
+				    // this callback will be called asynchronously
+				    // when the response is available
+
+				  }, function errorCallback(response) {
+				    console.log('failure response', response.status)
+				    // called asynchronously if an error occurs
+				    // or server returns response with an error status.
+				  });
+
+				    console.log('changed1', obj.enabled);
+				  };  
+			 
+			 
 $scope.stateChanged = function (qId) {
    if($scope.myData[qId]){ //If it is checked
        alert('test');
-   }
-}
-  $http.get("./data.json").success(function (response) {
-      $scope.myData = (response.ReadFeaturesResponse.feature);
-  });
+   } }
+//  $http.get("script/data.json").success(function (response) {
+//      $scope.myData = (response.ReadFeaturesResponse.feature);
+//  });
 });
