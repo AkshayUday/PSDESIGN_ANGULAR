03de95e8b97347229ff0fd2ce6cc21592338f9c9
Akshay Uday <auakshay123@gmail.com>
2017-11-04 14:27:40 -0400

    11/04 update

--- globalFeatures.js
+++ globalFeatures.js
@@ -14,7 +14,7 @@ var states = [
       name: 'FEATURES',
       state: {
         url: "/",
-        templateUrl: 'script/feature.html',
+        templateUrl: '../script/feature.html',
         controller: "globalCtrl"
       }
     },
@@ -22,7 +22,7 @@ var states = [
       name: 'PLANSPONSOR',
       state: {
         url: "#",
-        templateUrl: 'script/planSponsor.html',
+        templateUrl: '../script/planSponsor.html',
         controller: "globalCtrl"
       }
     }]
@@ -85,7 +85,13 @@ myapp.filter('myFilter', function () {
    };
 });
 
+
+
+
 myapp.controller('globalCtrl', function($scope, $http, $timeout, $window) {
+	
+	
+	
 	$scope.resData = ($window.cardData.ReadFeaturesResponse.feature);
 	$('.home-state a').removeClass('.active');
 	$('.feature-state a').addClass('.active');
@@ -98,7 +104,10 @@ myapp.controller('globalCtrl', function($scope, $http, $timeout, $window) {
   $scope.selected = 'View All';
   $scope.viewAll = true;
   $scope.categorydescription = 'View All';
+  
 
+  
+  
   $scope.show = function(selItemName,ind) {
     // $scope.viewAll = false;
 console.log('selected is ',selItemName);
@@ -116,6 +125,8 @@ console.log('selected is ',selItemName);
     }
 
   }
+  
+  
 
   $scope.test = function(v) {
 
@@ -135,8 +146,9 @@ console.log('selected is ',selItemName);
   $scope.counter = 0;
  /* $scope.toggleSuccess =false;
   $scope.toggleFailure = false;*/
-  $scope.change = function(obj, inx) {
-	  $scope.showMessage = inx;
+  $scope.change = function(obj, inx, showingitems) {
+	    $scope.showMessage = inx;
+	    $scope.showingitems = showingitems
 	  
   //  if (!obj.enabled) {
   //    $scope.showMessage = inx;
@@ -210,16 +222,46 @@ console.log('selected is ',selItemName);
    //   $scope.myData = $scope.resData;
  // });
 });
+
+
+
 myapp.directive('statusPopover', function($compile) {
     return {
         restrict: 'A',
 //        transclude: false,
 //        scope: {},
         controller: function($scope){
+//        	 var convertTimezone = function(date){
+//       		  var newDate = new Date(date.setTime(date.getTime() + (date.getTimezoneOffset() * 60000)));
+//       		  var offset = date.getTimezoneOffset()/60;
+//       		  var hours = date.getHours();
+//       		  newDate.setHours(hours - offset);
+//       		  return newDate;
+//       	  };
         	 $scope.setStatus =function(feature){
-        		 //var setFeatureStatus = JSON.stringify(feature.status);
-        		  $scope.statusoptions = feature.status;
+        		 $scope.statusOptions = {};
+        		 $scope.statusOptions.status = feature.status;
+        		// $scope.startTime = moment(feature.scheduledPeriod.datetimeBegin,'YYYY-MM-DD HH.mm.ss.SSSSSS').format();
+        		// $scope.endTime = moment(feature.scheduledPeriod.datetimeEnd,'YYYY-MM-DD HH.mm.ss.SSSSSS').format('MMMM Do YYYY, h:mm:ss a');
+        		 $scope.startTime =new Date(feature.scheduledPeriod.datetimeBegin);
+        		 $scope.endTime = new Date(feature.scheduledPeriod.datetimeEnd);
+        		 $scope.startMessage = moment($scope.startTime).format('MM/DD/YYYY hh:mm A');
+        		 $scope.endMessage = moment($scope.endTime).format('MM/DD/YYYY hh:mm A');
+        		 //var isoDate = new Date('yourdatehere').toISOString();
+        		 //$('.datepicker1').data("DateTimePicker").date(feature.scheduledPeriod.datetimeBegin);
         	  };
+        	  $scope.saveStatus = function(){
+        		  var datetimeStart = $('.start-input').val();
+        		  var datetimeEnd = $('.end-input').val();
+        		  if(Date.parse(datetimeStart) < Date.parse(datetimeEnd)){
+        			   //start is less than End
+        			  alert("yes");
+        			}else{
+        			   //end is less than start
+        				alert("no");
+        			}
+        	  }
+        	 
         },
         link: function(scope, element, attrs) {
         	 
@@ -231,17 +273,66 @@ myapp.directive('statusPopover', function($compile) {
                  placement: function(context, src) {
              		$(context).addClass('size');
              		return 'bottom';
-             	//}
              	}
              };
              element.popover(options);
-             $('html').off('click', function(e) {
-            	  if (typeof $(e.target).data('original-title') == 'undefined' &&
-            	     !$(e.target).parents().is('.popover.in')) {
-            	    $('[data-original-title]').popover('hide');
-            	  }
-             });
+             scope.cancel = function() {
+            	 element.popover('hide');
+            	 element.triggerHandler('click');
+            	 element.attr('disabled', false);
+             }
 
+        }
+      };
+    });
+myapp.directive('datePicker', function($compile) {
+    return {
+    	restrict: 'A',
+    	scope: {
+    	    setDateObj: '=ngModel'
+    	  },
+    	
+        link: function(scope, elem, attrs, ngModel) {
+        	
+        	
+//        	var datecontent = $(".datepicker1").html();
+//            var datecompiledContent = $compile(datecontent)(scope);
+//            var dateoptions = {
+//                content: datecompiledContent,
+//                orientation: "auto",
+//                todayHighlight: true,
+//                format: 'mm/dd/yy',
+//                autoclose: true
+//            	
+//            };
+//            elem.datepicker(dateoptions);
+//            console.log("executed");
+        	//elem.data("DatePicker").date(scope.setDate);
+        	//scope.setDate = $filter('date')(scope.setDate, "dd/MM/yyyy");
+//        	scope.setDateFormatted = new Date(scope.setDate);
+        	if(elem.hasClass(".start-date")){
+        		scope.setDateObj = scope.startTime;
+        	}
+        	else if(elem.hasClass(".end-date")){
+        		scope.setDateObj = scope.endTime;
+        	}
+        	elem.datetimepicker({
+        		defaultDate:scope.setDateObj,
+        		autoclose:true,
+        		
+  
+        		
+        	      
+//        		change:function (date) {
+//
+//                    // Triggers a digest to update your model
+//                    scope.$apply(function () {
+//                        ngModelCtrl.$setViewValue(date);
+//                    });
+//
+//                }
+        	});
+        	
         }
       };
     });
