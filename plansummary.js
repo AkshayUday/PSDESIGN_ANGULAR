d98b30390818b3820cc7a00e7a7dfc5484f6d813
Akshay Uday <auakshay123@gmail.com>
2017-12-09 23:06:09 -0500

    12/09/2017

--- plansummary.js
+++ plansummary.js
@@ -1,4 +1,12 @@
-myapp.controller('summaryCtrl',function($scope,$window,$state){
+myapp.controller('summaryCtrl',function($scope,$window,$state,$stateParams){
+//	var plansponsorState = function(){
+//		console.log($state);
+//	}
+	
+	
+	
+	
+	console.log('getting'+$stateParams.obj);
 	
 	$scope.psName = "Aetna";
     $scope.psuId = "12345";
@@ -12,31 +20,161 @@ myapp.controller('summaryCtrl',function($scope,$window,$state){
     	$state.go('PLANSPONSOR.SPONSOR');
     }
     $scope.viewAllCount = 124;
-    $scope.resData = ($window.cardData.ReadFeaturesResponse.feature);
-    $scope.myData = $scope.resData;
-    $scope.showItems = 'View All';
+    $scope.resData = {
+    		  "getCategoryFeaturesResponse": {
+    			    "ReadCategoryFeaturesResponse": {
+    			      "totalFeaturesCount": 31,
+    			      "featureSet": [
+    			       
+    			        {
+    			          "featuresCount": 1,
+    			          "category": {
+    			            "categoryName": "Benefits",
+    			            "categoryDescription": "Benefits"
+    			          },
+    			          "features": [
+    			            {
+    			              "featureIdentifier": {
+    			                "resourceId": "110004~BenTracker",
+    			                "idValue": "BenTracker"
+    			              },
+    			              "description": "Benefits Tracker",
+    			              "name": "Benefits Tracker"
+    			            }
+    			          ]
+    			        },
+    			       
+    			        {
+    			          "featuresCount": 3,
+    			          "category": {
+    			            "categoryName": "Provider",
+    			            "categoryDescription": "Provider"
+    			          },
+    			          "features": [
+    			            {
+    			              "featureIdentifier": {
+    			                "resourceId": "110004~BHCarveOut",
+    			                "idValue": "BHCarveOut"
+    			              },
+    			              "description": "Excludes behavioral health specialties/procedures in search and allows front end display rules on BH providers via name search",
+    			              "name": "Behavioral Health Carve Out"
+    			            },
+    			            {
+    			              "featureIdentifier": {
+    			                "resourceId": "110004~ProviderSearch",
+    			                "idValue": "ProviderSearch"
+    			              },
+    			              "description": "Enables member to search for a provider by specialty or name",
+    			              "name": "Provider Search"
+    			            },
+    			            {
+    			              "featureIdentifier": {
+    			                "resourceId": "110004~Telemedicine",
+    			                "idValue": "Telemedicine"
+    			              },
+    			              "description": "Enables member to view and price telemedicine providers",
+    			              "name": "Telemedicine"
+    			            }
+    			          ]
+    			        }
+    			      ]
+    			    }
+    			  },
+    			  "getCategoryFeaturesCode": 200,
+    			  "getToggleInfo": {
+    			    "getToggleLevelCode": 200,
+    			    "getPlanSponsorResponse": {
+    			      "ReadFeaturesResponse": {
+    			        "featureSet": [
+    			          {
+    			            "featureLevelId": "AA",
+    			            "features": [
+    			              {
+    			                "enabled": true,
+    			                "resourceId": "110004~AccExpanded"
+    			              },
+    			              {
+    			                "enabled": true,
+    			                "resourceId": "110004~AetnaHealthEnabled"
+    			              }
+    			              
+    			            ]
+    			          },
+    			          {
+    			            "featureLevelId": "AB",
+    			            "features": [
+    			              {
+    			                "enabled": true,
+    			                "resourceId": "110004~AccExpanded"
+    			              },
+    			              {
+    			                "enabled": true,
+    			                "resourceId": "110004~AetnaHealthEnabled"
+    			              }
+    			            ]
+    			          },
+    			          {
+    			            "featureLevelId": "AC",
+    			            "features": [
+    			              {
+    			                "enabled": true,
+    			                "resourceId": "110004~AccExpanded"
+    			              },
+    			              {
+    			                "enabled": true,
+    			                "resourceId": "110004~AetnaHealthEnabled"
+    			              }
+    			            ]
+    			          }
+    			        ]
+    			      }
+    			    }
+    			  }
+    			};
+    
+    
+    
+    
+   
+    
+    $scope.myData = $stateParams.obj.getCategoryFeaturesResponse.ReadCategoryFeaturesResponse.featureSet;
+    $scope.toggleData = $stateParams.obj.getToggleInfo.getPlanSponsorResponse.ReadFeaturesResponse.featureSet;
+    
+    
+    console.log($scope.toggleData,'texttttttt');
+    $scope.showItems = 'View All';   
     $scope.showPopover=false;
     $scope.expand=false;
     $scope.selected = 'View All';
     $scope.viewAll = true;
     $scope.categorydescription = 'View All';
     $scope.featureCount = 60;
-    $scope.show = function(selItemName, ind){
+    
+    $scope.showFeature = function(selItemName, ind){
     // $scope.viewAll = false;
     console.log('selected is ',selItemName);
     if(selItemName == 'View All'){
 	      $scope.viewAll = true;
 	      $scope.showItems = 'View All';
+	     var featureSets = $scope.myData;
+	     $scope.selectedData = [];
+	      angular.forEach(featureSets, function (featureSet) {
+	    	  var featureObjs= featureSet.features;
+	    	  angular.forEach(featureObjs, function (featureObj) {
+	    		  $scope.selectedData.push(featureObj);
+	    	  });
+	    	});
 //	      $scope.myData = $scope.resData;
 //	      $scope.categorydescription = 'View All';
 	       $scope.selected = ind;
     	}else{
 	        $scope.viewAll = false;
+	        $scope.selectedData = selItemName.features;
 //	         $scope.categorydescription = selItemName.featureCategory.categoryDescription;
 //	         $scope.featureCount = 40;
 	         $scope.selected = ind;
     	}
     	$window.scrollTo(0, angular.element('.shadow-box').offsetTop);
     }
- 
+    $scope.showFeature('View All','showAllInx');
     });
