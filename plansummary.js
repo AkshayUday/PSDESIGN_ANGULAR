153633ce649c45fa9ecbe0ccfcf2cf513c8e25af
Akshay Uday <auakshay123@gmail.com>
2017-12-14 12:01:04 -0500

    12/14/2017

--- plansummary.js
+++ plansummary.js
@@ -1,20 +1,19 @@
-myapp.controller('summaryCtrl',function($scope,$window,$state,$stateParams){
+myapp.controller('summaryCtrl',function($scope,$window,$state,$stateParams,$filter){
 //	var plansponsorState = function(){
 //		console.log($state);
 //	}
 	
 	
 	
+	/*
+	$scope.errorData = $stateParams.obj;
+	$scope.errorSet = true;
+	
 	
 	console.log('getting'+$stateParams.obj);
 	
 	$scope.psName = "Aetna";
     $scope.psuId = "12345";
-    $scope.summary = {
-        fundingType: "Self Insured",
-        benefitType: "Medical",
-        effecitveDate : "01/01/2017 to 12/31/2017"
-    };
     $scope.stateChange = function()
     {
     	$state.go('PLANSPONSOR.SPONSOR');
@@ -132,49 +131,225 @@ myapp.controller('summaryCtrl',function($scope,$window,$state,$stateParams){
     			  }
     			};
     
+    */
     
     
-    
-   
-    
-    $scope.myData = $stateParams.obj.getCategoryFeaturesResponse.ReadCategoryFeaturesResponse.featureSet;
-    $scope.toggleData = $stateParams.obj.getToggleInfo.getPlanSponsorResponse.ReadFeaturesResponse.featureSet;
-    
-    
-    console.log($scope.toggleData,'texttttttt');
-    $scope.showItems = 'View All';   
-    $scope.showPopover=false;
-    $scope.expand=false;
-    $scope.selected = 'View All';
-    $scope.viewAll = true;
-    $scope.categorydescription = 'View All';
-    $scope.featureCount = 60;
-    
-    $scope.showFeature = function(selItemName, ind){
-    // $scope.viewAll = false;
-    console.log('selected is ',selItemName);
-    if(selItemName == 'View All'){
-	      $scope.viewAll = true;
-	      $scope.showItems = 'View All';
-	     var featureSets = $scope.myData;
-	     $scope.selectedData = [];
-	      angular.forEach(featureSets, function (featureSet) {
-	    	  var featureObjs= featureSet.features;
-	    	  angular.forEach(featureObjs, function (featureObj) {
-	    		  $scope.selectedData.push(featureObj);
-	    	  });
-	    	});
-//	      $scope.myData = $scope.resData;
-//	      $scope.categorydescription = 'View All';
-	       $scope.selected = ind;
-    	}else{
-	        $scope.viewAll = false;
-	        $scope.selectedData = selItemName.features;
-//	         $scope.categorydescription = selItemName.featureCategory.categoryDescription;
-//	         $scope.featureCount = 40;
-	         $scope.selected = ind;
-    	}
-    	$window.scrollTo(0, angular.element('.shadow-box').offsetTop);
+   if(typeof($stateParams.obj) == "string"){
+       
+	   $scope.psErrorData = $stateParams.obj;
+		$scope.hideTable = true;
+		$scope.psumryError = true;
+   }
+   else if(typeof($stateParams.obj) == "boolean"){
+	  
+	  	$scope.hideTable = true;
+	  	$scope.systemDown = true;
+	   
+   }else{
+	   $scope.psumryError = false;
+	   $scope.hideTable = false;
+	  	$scope.systemDown = false;	  
+	    $scope.stateChange = function()
+	    {
+	    	$state.go('PLANSPONSOR.SPONSOR',{source:'summary'});
+	    }
+	    $scope.summaryObject = $stateParams.obj || JSON.parse(localStorage.getItem('summaryResponse'));
+	    $scope.catFeatures=$scope.summaryObject.getCategoryFeaturesResponse.ReadCategoryFeaturesResponse; 
+		$scope.psName = $scope.summaryObject.nameFull;
+	    $scope.psuId = $scope.summaryObject.planSponsorId;
+	    console.log('PSUID',$scope.summaryObject)
+	    $scope.myData = ($scope.summaryObject.getCategoryFeaturesResponse.ReadCategoryFeaturesResponse.featureSet);
+	    $scope.toggleData = $scope.summaryObject.getToggleInfo.getPlanSponsorResponse.ReadFeaturesResponse.featureSet;
+	    console.log($scope.myData,'texttttttt');
+	    $scope.showItems = 'View All';   
+	    $scope.showPopover=false;
+	    $scope.expand=false;
+	    $scope.selected = 'View All';
+	    $scope.viewAll = true;
+	    $scope.categorydescription = 'View All';
+	    $scope.isFirstPage = true;
+	     // findByKey is used to find an object inside an array
+	 // of object, by the value of a key.
+	 function findByKey (array, key, value,outkey) {
+	 	return array.filter(function (obj) {
+//console.log('object',key,obj);
+	 		return  obj[key] === value;
+	 	})[0];
+	 }
+	 
+	
+	    
+	    $scope.showFeature = function(selItemName, ind){
+	    // $scope.viewAll = false;
+	    console.log('selected is ',selItemName);
+	    if(selItemName == 'View All'){
+		      $scope.viewAll = true;
+		      $scope.showItems = 'View All';
+		      var featureSets = $scope.myData;
+
+			  $scope.viewAllCount = $scope.catFeatures.totalFeaturesCount
+		      $scope.selectedData = [];
+		      $scope.serialData = [];
+		      angular.forEach(featureSets, function (featureSet) {
+		    	  var featureObjs= featureSet.features;
+		    	  angular.forEach(featureObjs, function (featureObj) {
+		    		  $scope.serialData.push(featureObj);
+		    	  });
+		    	});
+		      
+		      $scope.selectedData = $scope.serialData.map(function (group) {
+					console.log('inside',$scope.toggleData,$scope.toggleData[0].featureLevelId);
+					$scope.toggleData.map(function(data){
+						return {
+							resourceId: group.resourceId,
+							enabled: findByKey(data.features , 'resourceId', group.featureIdentifier.resourceId, 'resourceName'),
+							name: group.name,
+							featureLevelId: data.featureLevelId
+							
+						};
+					})
+				
+			});
+				
+			$scope.selectedData = $filter('orderBy')($scope.selectedData,'name');
+			console.log('selected data is ',$scope.selectedData)
+		      
+		      
+//		      $scope.myData = $scope.resData;
+//		      $scope.categorydescription = 'View All';
+		       $scope.selected = ind;
+	    	}else{
+		        $scope.viewAll = false;
+		        $scope.isFirstPage = true;
+		        $scope.serialData = selItemName.features;
+		        $scope.selectedData = $scope.serialData.map(function (group) {
+					console.log('inside',group);
+				return {
+					resourceId: group.resourceId,
+					enabled: findByKey($scope.toggleData[0].features , 'resourceId', group.featureIdentifier.resourceId, 'resourceName'),
+					name: group.name
+					
+				};
+			});
+		        console.log('testdata',$scope.selectedData);
+//		         $scope.categorydescription = selItemName.featureCategory.categoryDescription;
+//		         $scope.featureCount = 40;
+		         $scope.selected = ind;
+	    	}
+	    	$scope.$broadcast('categoryChangedEvent');
+	    	$window.scrollTo(0, angular.element('.shadow-box').offsetTop);
     }
-    $scope.showFeature('View All','showAllInx');
+	    $scope.showFeature('View All','showAllInx');
+   }
     });
+
+myapp.directive('planSummaryDetails',function(){
+	return{
+		
+		restrict: 'E',
+		scope:{},
+		controller:function($scope){
+			$scope.summary = {
+		        fundingType: "Self Insured",
+		        benefitType: "Medical",
+		        effecitveDate : "01/01/2017 to 12/31/2017"
+		    };
+		},
+		templateUrl: '../script/planSummaryDetails.html'
+	};
+	
+});
+
+myapp.directive('summaryFeatureDetails',function(){
+	return{
+		restrict: 'E',
+		scope:true,
+		controller:function($scope){
+			$scope.columnPerPage = 5;
+			$scope.currentPage = 0;
+			$scope.currentColumns = [];
+			$scope.totalNumberOfPage = Math.floor($scope.selectedData.length/$scope.columnPerPage);
+			if (($scope.selectedData.length % $scope.columnPerPage) > 0){
+				$scope.totalNumberOfPage = $scope.totalNumberOfPage + 1;
+			}
+			
+			console.log($scope.totalNumberOfPage);
+
+			$scope.currentColumns = $scope.selectedData.slice($scope.columnPerPage*$scope.currentPage,Math.min($scope.selectedData.length, $scope.columnPerPage));
+			
+			$scope.$on('categoryChangedEvent',function(){
+				if($scope.isFirstPage){
+
+					$scope.totalNumberOfPage = Math.floor($scope.selectedData.length/$scope.columnPerPage);
+					if (($scope.selectedData.length % $scope.columnPerPage) > 0){
+						$scope.totalNumberOfPage = $scope.totalNumberOfPage + 1;
+					}
+					
+					$scope.currentPage = 0;
+					$scope.currentColumns = $scope.selectedData.slice($scope.columnPerPage*$scope.currentPage,Math.min($scope.selectedData.length, $scope.columnPerPage));
+				}
+			});
+			
+			$scope.next = function(){
+				if ($scope.currentPage <= $scope.totalNumberOfPage -2){
+					$scope.currentPage = $scope.currentPage +1 ;
+					//$scope.currentColumns.length = 0;
+					//$scope.isFirstPage = false;
+					if ($scope.currentColumns.length > 0){
+						
+						while($scope.currentColumns.length > 0){
+							$scope.currentColumns.pop();
+						}
+						
+						$scope.currentColumns = [];
+					}
+					
+					if (($scope.currentPage === $scope.totalNumberOfPage - 1 ) 
+							&& ($scope.selectedData.length % $scope.columnPerPage) > 0){
+						for (var i = $scope.columnPerPage*$scope.currentPage; 
+									i < ($scope.columnPerPage*$scope.currentPage) + 
+									($scope.selectedData.length % $scope.columnPerPage); i++){
+							$scope.currentColumns.push($scope.selectedData[i]);
+						}
+					} else {
+					
+						for (var i = $scope.columnPerPage*$scope.currentPage; i < $scope.columnPerPage*$scope.currentPage + $scope.columnPerPage ; i++){
+							$scope.currentColumns.push($scope.selectedData[i]);
+						}
+					}
+					
+				}
+			}
+			
+			$scope.prev = function(){
+				if($scope.currentPage >=1){
+					$scope.currentPage = $scope.currentPage -1 ;
+					//$scope.currentColumns.length = 0;
+					//$scope.isFirstPage = false;
+					if ($scope.currentColumns.length > 0){
+						
+						while($scope.currentColumns.length > 0){
+							$scope.currentColumns.pop();
+						}
+						
+						$scope.currentColumns = [];
+					}
+					
+					for (var i = $scope.columnPerPage*$scope.currentPage; i < $scope.columnPerPage*$scope.currentPage + $scope.columnPerPage ; i++){
+						$scope.currentColumns.push($scope.selectedData[i]);
+					}
+					
+				}
+			}
+			
+		},
+		templateUrl:'../script/featureDetails.html'
+	};
+});
+
+myapp.directive('summaryCategoryList',function(){
+	return {
+		restrict:'E',
+		templateUrl:'../script/categoryList.html'
+	}
+});
